920e2bd7c10c2d3b2f54dd47aadeff81dec90295
Alejandro Estrada Hernandez <telematik_4@hotmail.com>
2023-10-28 10:01:11 -0400

    Create parameterizedDSL.groovy

---
+++ parameterizedDSL.groovy
@@ -0,0 +1,40 @@
+job('example2-job-dsl') {
+  description('Example JOB DSL for Jenkins Course')
+  scm {
+    git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
+      node / gitConfigName('Candoyeya')
+      node / gitConfigEmail('telematik_4@hotmail.com')
+    }
+  }
+  parameters {
+  	stringParam('nombre', defaultValue = 'Alejandro', description = 'Parametro de cadena para el Job Booleano')
+    choiceParam('planeta', ['Mercurio','Venus','Tierra','Marte','Jupiter','Saturno','Urano','Neptuno'])
+    booleanParam('agente', false)
+  }
+  triggers {
+    cron('H/3 * * * *')
+  }
+  steps {
+  	shell("bash jobscript.sh")
+  }
+  publishers {
+  	mailer('telematik_4@hotmail.com', true, true)
+    slackNotifier {
+      notifyAborted(true)
+      notifyEveryFailure(true)
+      notifyNotBuilt(false)
+      notifyUnstable(false)
+      notifyBackToNormal(true)
+      notifySuccess(false)
+      notifyRepeatedFailure(false)
+      startNotification(false)
+      includeTestSummary(false)
+      includeCustomMessage(false)
+      customMessage(null)
+      sendAs(null)
+      commitInfoChoice('NONE')
+      teamDomain(null)
+      authToken(null)
+    }
+  }
+}
